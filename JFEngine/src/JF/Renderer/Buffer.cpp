2ae110ccced0e3105a7b1c68b10384bad1eb883c
W <as5818956@gmail.com>
2020-06-18 23:25:35 +0900

    upgrade

--- JFEngine/src/JF/Renderer/Buffer.cpp
+++ JFEngine/src/JF/Renderer/Buffer.cpp
@@ -12,7 +12,7 @@ namespace JF {
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:    JF_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:  return  std::make_shared<OpenGLVertexBuffer>(vertices, size);
+		case RendererAPI::API::OpenGL:  return  CreateRef<OpenGLVertexBuffer>(vertices, size);
 		}
 
 		JF_CORE_ASSERT(false, "Unknown RendererAPI!");
@@ -24,7 +24,7 @@ namespace JF {
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:    JF_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLIndexBuffer>(indices, size);
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLIndexBuffer>(indices, size);
 		}
 		JF_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
